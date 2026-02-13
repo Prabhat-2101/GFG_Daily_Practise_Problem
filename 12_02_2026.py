dd83e093e72163dc3797b2f0ad53653b5fdb3292
Prabhat Kumar Raj <118561758+Prabhat-2101@users.noreply.github.com>
2026-02-13 21:06:12 +0530

    Create 12_02_2026.py

---
+++ 12_02_2026.py
@@ -0,0 +1,29 @@
+# https://www.geeksforgeeks.org/problems/all-numbers-with-specific-difference3558/1
+class Solution:
+    def sum_digits(self, n):
+        s = 0
+        while n>0:
+            s += n%10
+            n //= 10
+        return s
+    
+    def getCount(self, n, d):
+        if n<=9:
+            return 0;
+        
+        left, right = 1, n
+        start = -1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if mid - self.sum_digits(mid) >= d:
+                start = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        if start == -1:
+            return 0
+        
+        return n - start + 1
