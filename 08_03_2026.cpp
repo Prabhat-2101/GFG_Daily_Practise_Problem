0600c1cb3fb7dd1ab87b67f7230b410ebebe22dd
Prabhat Kumar Raj <118561758+Prabhat-2101@users.noreply.github.com>
2026-03-08 17:47:51 +0530

    Update 08_03_2026.cpp

--- 08_03_2026.cpp
+++ 08_03_2026.cpp
@@ -1,19 +1,23 @@
-//https://www.geeksforgeeks.org/problems/dice-throw5349/1
+//https://www.geeksforgeeks.org/problems/pythagorean-triplet3018/1
 class Solution {
   public:
-    vector<vector<int>> dp;
-    int helper(int m,int n,int x){
-        if(n==0 and x==0) return 1;
-        if(n<=0 or x<=0) return 0;
-        if(dp[n][x] != -1) return dp[n][x];
-        int count = 0;
-        for(int i=1;i<=m;i++){
-            count += helper(m,n-1,x-i);
+    bool pythagoreanTriplet(vector<int>& arr) {
+        int size = arr.size();
+        unordered_set<int> st;
+        for(int &it: arr){
+            st.insert(it);
+            it = it*it;
         }
-        return dp[n][x] = count;
-    }
-    int noOfWays(int m, int n, int x) {
-        dp.resize(n+1,vector<int>(x+1,-1));
-        return helper(m,n,x);
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                int num = arr[i] + arr[j];
+                int root = sqrt(num);
+                if(root * root == num){
+                    if(st.find(root) != st.end())
+                        return true;
+                }
+            }
+        }
+        return false;
     }
 };
